2c3b0b92564bed8aafa964c2d654f44abd4f7ebd
rackman69 <91697842+rackman69@users.noreply.github.com>
2023-11-17 20:35:39 -0500

    Add files via upload
    Now reads data continually from a data file. Also added a sample graph and data readouts.

--- DataRead.py
+++ DataRead.py
@@ -1,27 +1,88 @@
-import os
-import os.path
+class Data:
+    def __init__(self):
+        self.InternalData = []
+        self.x = []
+        self.y = []
+
+        self.graphx = []
+        self.graphy = []
+
+        self.LatestElement = 0
+
+        #appends the 1D arrays for the individual components for the coordinates to the 2D array that encompasses both
+        self.InternalData.append(self.x)
+        self.InternalData.append(self.y)
+        self.InternalData.append(self.graphx)
+        self.InternalData.append(self.graphy)
+
+        
+
+
+def FileReadSequential(Dataset):
+    with open('data.txt', "r") as f:
+
+
+        CoordArrayList = f.readlines() #reads each line of the data file and splits it into a array list
+
+        lastLine = Dataset.LatestElement
+
+        CoordArrayList[lastLine] = CoordArrayList[lastLine].strip("\n")
+        SplitArray = CoordArrayList[lastLine].split() #splits each line into the individual x and y components and records it
+        Dataset.x.insert(lastLine,SplitArray[0]) 
+        Dataset.y.insert(lastLine,SplitArray[1])
+        Dataset.graphx.insert(lastLine,SplitArray[2]) 
+        Dataset.graphy.insert(lastLine,SplitArray[3])
+
+        
+
+        if ((len(CoordArrayList) - 1) > (Dataset.LatestElement)):
+            Dataset.LatestElement += 1
+
+
+        
+        
+
+        f.close()
+
+    return Dataset
+
+
 
 def fileRead():
     with open('data.txt', "r") as f:
 
-        Coordinates = []
+        Dataset = []
         x = []
         y = []
 
+        graphx = []
+        graphy = []
+
+        
+
+
         #appends the 1D arrays for the individual components for the coordinates to the 2D array that encompasses both
-        Coordinates.append(x)
-        Coordinates.append(y)
+        Dataset.append(x)
+        Dataset.append(y)
+        Dataset.append(graphx)
+        Dataset.append(graphy)
 
         CoordArrayList = f.readlines() #reads each line of the data file and splits it into a array list
     
-
+        
         for i in range(len(CoordArrayList)):
             CoordArrayList[i] = CoordArrayList[i].strip("\n")
             SplitArray = CoordArrayList[i].split() #splits each line into the individual x and y components and records it
             x.insert(i,SplitArray[0]) 
             y.insert(i,SplitArray[1])
+            graphx.insert(i,SplitArray[2]) 
+            graphy.insert(i,SplitArray[3])
+        
 
         f.close()
 
-    return Coordinates
+    return Dataset
+
+
+
 
--- MainGUI.py
+++ MainGUI.py
@@ -4,34 +4,43 @@ import PySide6.QtCore
 import PySide6.QtWidgets
 
 import time
+import numpy
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Graph
+
 
 import DataRead
 
-class MyWidget(QtWidgets.QWidget): #Class for general UI stuff (buttons and text)
+class MainWidget(QtWidgets.QMainWindow): #Class for general UI stuff (buttons and text)
     def __init__(self):
-        super().__init__()
+        super(MainWidget, self).__init__()
 
         self.setWindowTitle("Test Dot Mover")
-        self.resize(800,800)
+        self.resize(1800,800)
 
-        self.label = QtWidgets.QLabel()
-        self.label.setText("Enter Coordinates of where you want the dot to move")
-
-        self.TextBoxX = QtWidgets.QTextEdit()
-        self.TextBoxX.setFixedWidth(400)
-        self.TextBoxX.setFixedHeight(200)
-        
-        self.TextBoxY = QtWidgets.QTextEdit()
-        self.TextBoxY.setFixedWidth(400)
-        self.TextBoxY.setFixedHeight(200)
-
-        self.buttonLeft = QtWidgets.QPushButton()
-        self.buttonLeft.setText("Enter Coordinates")
+        self.label1 = QtWidgets.QLabel()
+        self.label1.setText("Acceleration: 0")
+        self.label2 = QtWidgets.QLabel()
+        self.label2.setText("Pressure: 0")
+        self.label3 = QtWidgets.QLabel()
+        self.label3.setText("Placeholder: 0")
+        self.label4 = QtWidgets.QLabel()
+        self.label4.setText("Placeholder: 0")
 
         self.buttonRight = QtWidgets.QPushButton()
         self.buttonRight.setText("Preset Coordinate List")
+        self.buttonLeft = QtWidgets.QPushButton()
+        self.buttonLeft.setText("Live Update")
+        
 
         #Graphical -------
+
+        #Graph ---
+
+        self.DataPlot = Matplot() 
+
+        #Graph ---
         
         #Painter Instance
         #self.map = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap("GTAAREA.jpg")) #A graphical object that takes in a pixelmap object
@@ -44,37 +53,128 @@ class MyWidget(QtWidgets.QWidget): #Class for general UI stuff (buttons and text
         self.scene.addItem(self.dot)
 
         self.view = QtWidgets.QGraphicsView(self.scene) #A object (window) to show the scene
-        self.view.setFixedWidth(800)
-        self.view.setFixedHeight(500)
+        self.view.setFixedWidth(1000)
+        self.view.setFixedHeight(750)
 
         #creates an object for a grid type window layout, above objects can be added to the layout
-        layout = QtWidgets.QGridLayout(self)
-        layout.addWidget(self.view, 0,0,1,2) #to be contained in cell row:0,column:0, spanning 1 row and 2 columns
-        layout.addWidget(self.label)
-        layout.addWidget(self.TextBoxX, 3 ,0)
-        layout.addWidget(self.TextBoxY,3 , 1)
-        layout.addWidget(self.buttonLeft,4, 0)
-        layout.addWidget(self.buttonRight, 4 ,1)
+        dataLayout = QtWidgets.QGridLayout() 
+        dataLayout.addWidget(self.DataPlot, 0, 0, 1 , 4) 
 
-        self.buttonLeft.clicked.connect(self.buttonLeftInteraction) #when interacted with, executes function within the parameter
-        self.buttonRight.clicked.connect(self.buttonRightInteraction) #when interacted with, executes function within the parameter
+
+        dataChildLayout = QtWidgets.QVBoxLayout()
         
+
+        dataChildLayout.addWidget(self.label1) 
+        dataChildLayout.addWidget(self.label2)
+        dataChildLayout.addWidget(self.label3)
+        dataChildLayout.addWidget(self.label4)
+
+        dataLayout.addLayout(dataChildLayout,1,0)
+        dataLayout.addWidget(self.buttonLeft, 2 ,1)
+        dataLayout.addWidget(self.buttonRight, 2 ,2)
+        
+
+        
+
+        widget = QtWidgets.QWidget(self)
+        
+
+        layoutMain = QtWidgets.QGridLayout()
+        layoutMain.addWidget(self.view, 0,0,8,3) #to be contained in cell row:0,column:0, spanning 1 row and 3 columns
+        
+        layoutMain.addLayout(dataLayout, 0, 3, 1, 2)
+
+        widget.setLayout(layoutMain)
+
+        self.setCentralWidget(widget)
+       
+
+        self.buttonRight.clicked.connect(self.buttonRightInteraction) #when interacted with, executes function within the parameter
+        self.buttonLeft.clicked.connect(self.buttonLeftInteraction) #when interacted with, executes function within the parameter
+
     @QtCore.Slot()
     def buttonLeftInteraction(self):
-        #using setpos method and the float casted contents of the textboxes, the dot location can then be changed
-        self.dot.setPos(float(self.TextBoxX.toPlainText()),float(self.TextBoxY.toPlainText()))
         
-    @QtCore.Slot()
-    def buttonRightInteraction(self):
-        CoordinateArray = DataRead.fileRead() #A 2D array for x,y coordinates
+
+        DataSet = DataRead.Data() 
         
-        for i in range(len(CoordinateArray[0])):
+        Latest = DataRead.FileReadSequential(DataSet) #A 2D array for x,y coordinates
+
+        
+        
+        #for i in range(13): #temporary, gonna make a while loop later and another button to terminate the file checking
+
+        while True:
 
             QtCore.QCoreApplication.processEvents() #apparently a "bad" way to update the gui as the for loop progresses. Need to figure out how to use the built in Qthread/multithreading to do it properly
+
+            i = DataSet.LatestElement -1
             
-            self.dot.setPos(float(CoordinateArray[0][i]),float(CoordinateArray[1][i])) #sets x and y from CoordinateArray
-            print (CoordinateArray[0][i] + "," + CoordinateArray[1][i]) #debug
+            self.dot.setPos(float(DataSet.InternalData[2][i]),float(DataSet.InternalData[3][i])) #sets x and y from CoordinateArray
+            print (DataSet.InternalData[2][i] + "," + DataSet.InternalData[3][i]) #debug
+
+            
+            self.DataPlot.axes.scatter(float(DataSet.InternalData[2][i]), float(DataSet.InternalData[3][i]))
+            self.DataPlot.draw()
+
+            self.label1.setText("Acceleration:" + DataSet.InternalData[2][i])
+            self.label2.setText("Pressure:" + DataSet.InternalData[3][i])
+
+            Latest = DataRead.FileReadSequential(DataSet) #A 2D array for x,y coordinates
+
+            
+            
+
             time.sleep(1)
+        
+   
+    @QtCore.Slot()
+    def buttonRightInteraction(self):
+        DataSet = DataRead.fileRead() #A 2D array for x,y coordinates
+
+        
+        
+        for i in range(len(DataSet[0])):
+
+           
+            
+            self.dot.setPos(float(DataSet[0][i]),float(DataSet[1][i])) #sets x and y from CoordinateArray
+            print (DataSet[0][i] + "," + DataSet[1][i]) #debug
+
+            #self.DataPlot.axes.clear()
+            self.DataPlot.axes.scatter(float(DataSet[2][i]), float(DataSet[3][i]))
+            self.DataPlot.draw()
+
+            self.label1.setText("Placeholder:" + DataSet[2][i])
+            self.label2.setText("Placeholder:" + DataSet[3][i] )
+
+
+
+            QtCore.QCoreApplication.processEvents() #apparently a "bad" way to update the gui as the for loop progresses. Need to figure out how to use the built in Qthread/multithreading to do it properly
+
+            time.sleep(1)
+
+
+        
+
+class Matplot(Graph):
+
+    def __init__(self):
+        super(Matplot, self).__init__(Figure())
+
+        self.figure = Figure()
+        self.canvas = Graph (self.figure)
+        self.axes = self.figure.add_subplot(111) #tf is 111?
+
+        self.axes.set_xlabel("Time")
+        self.axes.set_ylabel("Placeholder")
+        self.axes.set_title("Graph")
+
+        #self.DataPlot.axes.clear() #Reset
+
+
+
+
 
 
 #class ActionThread ()
@@ -83,11 +183,13 @@ class MyWidget(QtWidgets.QWidget): #Class for general UI stuff (buttons and text
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    MainWindow = MainWidget()
+
 
 
     
-    widget.show()
+    MainWindow.show()
+
 
     sys.exit(app.exec())
 
