94b61a786462bb61e352c4b84ebd277fad89636c
rackman69 <jacky.zhang1337@gmail.com>
2025-03-20 11:49:56 -0400

    Working Flask Skeleton

--- Python Backend/main.py
+++ Python Backend/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/test')
 def hello():
