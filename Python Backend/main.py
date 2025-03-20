376bb24cab11470c4670d8089522fd7a87291118
rackman69 <jacky.zhang1337@gmail.com>
2025-03-20 16:30:49 -0400

    more work done on flask backend

--- Python Backend/main.py
+++ Python Backend/main.py
@@ -1,30 +1,72 @@
 from flask import Flask, jsonify, request, current_app
-from flask_cors import CORS
 
 import populate
 import DataRead
 
+import datetime
+
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/test')
 def hello():
     return "Hello World!"
 
-@app.route('/readLast')
-def read():
-    return "Hello World!"
+@app.route('/serverstatus', methods=['GET'])
+def status():
+    with app.app_context(): 
+        deltatime = ((datetime.datetime.now()) - current_app.config['start_time'] ).seconds
+        
+        status = [{'running': True, 'runTime': deltatime}]
+        return jsonify(status)
+    
+@app.route('/radio/restart', methods=['GET'])
+def status():
+    with app.app_context(): 
+        #TO DO
+        return None
+    
+@app.route('/radio/stop', methods=['GET'])
+def status():
+    with app.app_context(): 
+        #TO DO
+        return None
+    
+@app.route('/radio/start', methods=['GET'])
+def status():
+    with app.app_context(): 
+        #TO DO
+        return None
+
+@app.route('/read/last/<int:index>', methods=['GET'])
+def readlast(index):
+    with app.app_context(): #we can access session data through app.app_context()
+        if (index == 0): #return all store data in a list (http://127.0.0.1:5000/read/last/0)
+            change = current_app.data_read.has_changed()
+            data = [{'latest_data_points': current_app.data_read.get_latest_point(), 'has_changed_since_last_request': change }]
+            return jsonify(data) #jsonify returns data formatted into json (good for parsing it from the frontend)
+        else:
+            change = current_app.data_read.has_changed()
+            data = [{'latest_data_points': current_app.data_read.get_point(index), 'has_changed_since_last_request': change }]
+            return jsonify(data) #jsonify returns data formatted into json (good for parsing it from the frontend)
+    
+@app.route('/read/gps', methods=['GET'])
+def readgps():
+    #TO DO
+    return None
+
+@app.route('/read/graph', methods=['GET'])
+def readgraphdata():
+    #TO DO
+    return None
 
 def create_app(): #persistant object handling
-    app = Flask(__name__)
-    with app.app_context():
-        current_app.data_read = DataRead.DataRead()
-        current_app.radio_read = populate.Populate() 
-        current_app.config['test'] = 'test' 
+    with app.app_context(): #define new objects here stored in current server session
+        current_app.data_read = DataRead.DataRead() 
+        current_app.radio_read = populate.Populate(current_app.data_read) 
+        current_app.radio_read.runpopulatefile() #run the radio data retrieval / test file
+        
+        current_app.config['start_time'] = (datetime.datetime.now())
 
-    with app.app_context():
-        #here to use current_app to get what you want.
-        print("placeholder")
 
 
 if __name__ == '__main__':
